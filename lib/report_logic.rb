b2e7286f881b6fa5f9a3f8ce8a5593384d1f3634
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-09-17 14:29:56 -0300

    Removing Grouper

--- lib/report_logic.rb
+++ lib/report_logic.rb
@@ -3,7 +3,6 @@ module ReportLogic
   autoload :Decorable    , 'report_logic/decorable'
   autoload :Decorator    , 'report_logic/decorator'
   autoload :Field        , 'report_logic/field'
-  autoload :Grouper      , 'report_logic/grouper'
   autoload :Session      , 'report_logic/session'
   autoload :I18nDecorator, 'report_logic/i18n_decorator'
   autoload :I18nSupport  , 'report_logic/i18n_support'
